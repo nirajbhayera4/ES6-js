5af6d316d742410a2f5b938f54664de6526b56e1
niraj <nirajbhayera4@gmail.com>
2026-09-11 20:32:10 +0530

    Add ES6 notes scaffold
    Create the initial HTML page and JavaScript file for ES6 basics. The project introduces let/const behavior, notes on variable mutability, and a starting point for arrow function examples.

---
+++ script.js
@@ -0,0 +1,19 @@
+let a=12;
+a=23;
+const b=100;
+b=200;
+
+
+//let and const dono se value store kr skte hai 
+//but dono differently behave krte hai 
+// let can change 
+//const cant change 
+
+//it is not necessary to declare the value to the let 
+// but is necessary to declare the value to the const 
+
+
+//ARROW FUNCTIONS- arrow functions 
+
+//1. fat arrow function
+
