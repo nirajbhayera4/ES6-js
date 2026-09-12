ef96daa31b8c02ac4b02f272464b893cebd535f2
niraj <nirajbhayera4@gmail.com>
2026-09-12 11:08:59 +0530

    Add basic OOP examples (encapsulation, inheritance, polymorphism)
    Adds three JS examples demonstrating OOP concepts: encapsulation (bankacc with private #bal, deposit/getbalance), inheritance (animal -> dog with eat and bark) and polymorphism (Animal base with Dog/Cat overriding sound). Includes console.log calls to show behavior. Note: duplicate identifier/class names (reused 'animal'/'dog') are present and may cause parse errors and should be renamed.

--- practice.js
+++ practice.js
@@ -13,3 +13,61 @@ student1.study();
 
 //encapsulation
 
+class bankacc{
+    #bal=0;
+    deposit(amount){
+        this.#bal+=amount;
+
+    }
+    getbalance(){
+        return this.#bal;
+    }
+}
+const acc=new bankacc();
+acc.deposit(1000);
+console.log(acc.getbalance());
+
+//inheritance
+
+class animal{
+    eat(){
+        console.log("eating...");
+    }
+
+}
+class dog extends animal{
+    bark(){
+        console.log("barking...");
+    }
+}
+const dog=new dog();
+dog.eat();
+dog.bark();
+
+//polymorphism
+class animal{
+    sound(){
+        console.log("animal makes sound...");
+    }
+
+}
+class Dog extends animal{
+    sound(){
+        console.log("dog barks...");
+    }
+}
+class Cat extends animal{
+    sound(){
+        console.log("cat meows...");
+    }
+}
+const dog=new Dog();
+const cat=new Cat();
+
+dog.sound();
+cat.sound();
+
+
+
+
+
